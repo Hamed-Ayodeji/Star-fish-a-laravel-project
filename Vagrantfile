d0d0c6717bb32130c82bada2dbb7396c761eb6d0
Ayodeji Hamed <ayodejihamed@gmail.com>
2023-10-22 09:35:25 +0100

    made final changes

--- Vagrantfile
+++ Vagrantfile
@@ -12,19 +12,20 @@ Vagrant.configure("2") do |config|
       vb.memory = "1024"
       vb.cpus = "1"
     end
-    master.vm.provision "shell", path: "./ansible-playbook/deploy.sh"
     master.vm.provision "shell", inline: <<-SHELL
-      ssh_config_file="/etc/ssh/sshd_config"
-      sudo sed -i 's/^PasswordAuthentication yes/PasswordAuthentication no/' "$ssh_config_file"
-      sudo sed -i 's/^#PubkeyAuthentication yes/PubkeyAuthentication yes/' "$ssh_config_file"
-      sudo systemctl restart ssh || sudo service ssh restart
+    ssh_config_file="/etc/ssh/sshd_config"
+    sudo sed -i 's/^PasswordAuthentication yes/PasswordAuthentication no/' "$ssh_config_file"
+    sudo sed -i 's/^#PubkeyAuthentication yes/PubkeyAuthentication yes/' "$ssh_config_file"
+    sudo systemctl restart ssh || sudo service ssh restart
+    sudo apt-get install -y avahi-daemon
     SHELL
+    master.vm.provision "shell", path: "./ansible-playbook/deploy.sh"
   end
   config.vm.define "Slave" do |slave|
     slave.vm.hostname = "Slave"
     slave.vm.network "private_network", ip: "192.168.56.21"
     slave.vm.provider "virtualbox" do |vb|
-      vb.memory = "4096"
+      vb.memory = "1024"
       vb.cpus = "2"
     end
     slave.vm.provision "shell", inline: <<-SHELL
@@ -32,6 +33,7 @@ Vagrant.configure("2") do |config|
       sudo sed -i 's/^PasswordAuthentication yes/PasswordAuthentication no/' "$ssh_config_file"
       sudo sed -i 's/^#PubkeyAuthentication yes/PubkeyAuthentication yes/' "$ssh_config_file"
       sudo systemctl restart ssh || sudo service ssh restart
+      sudo apt-get install -y avahi-daemon
     SHELL
   end
 end
--- ansible-playbook/deploy.sh
+++ ansible-playbook/deploy.sh
@@ -16,16 +16,18 @@ exec > >(tee -a "$log_file") 2>&1
 # Get server IP address
 server_ip=$(ip addr show eth1 | awk '/inet / {print $2}' | cut -d/ -f1)
 
+# Define variables for configurations
+server_admin_email="ayodejihamed@gmail.com"
+laravel_repo_url="https://github.com/laravel/laravel.git"
+
+#######################################################################################################
+#######################################################################################################
+
 # Start logging the script
 echo "========== Deployment started at $(date) =========="
 
-# Update the package list to ensure you download the latest versions of the packages
-echo "========== Updating the package list =========="
-apt-get update -y
-
-# Upgrade the installed packages to the latest versions
-echo "========== Upgrading the installed packages =========="
-apt-get upgrade -y
+#######################################################################################################
+#######################################################################################################
 
 # Add important repositories to the APT package manager
 echo "========== Adding important repositories to the APT package manager =========="
@@ -36,6 +38,15 @@ add-apt-repository -y ppa:ondrej/php
 echo "========== Updating the package list =========="
 apt-get update -y
 
+# Upgrade the installed packages to the latest versions
+echo "========== Upgrading the installed packages =========="
+apt-get upgrade -y
+
+#######################################################################################################
+#######################################################################################################
+
+## Install and setup AMP (Apache, MySQL, PHP) and other packages
+
 # Install the Apache web server
 echo "========== Installing the Apache web server =========="
 apt-get install -y apache2
@@ -44,8 +55,6 @@ apt-get install -y apache2
 echo "========== Starting and enabling the Apache web server =========="
 systemctl start apache2
 systemctl enable apache2
-
-#################################################### take a screenshot of test apache page
 
 # Generate a random secure password for MySQL root user
 echo "========== Generating a random secure password for MySQL root user =========="
@@ -88,10 +97,6 @@ systemctl restart apache2
 echo "========== Installing Composer =========="
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
-# Move the downloaded binary to the /usr/local/bin directory, so that you can run it with just the composer command
-echo "========== Moving the downloaded binary to the /usr/local/bin directory =========="
-mv composer.phar /usr/local/bin/composer
-
 # Configure Apache web server
 echo "========== Configuring Apache web server =========="
 
@@ -99,7 +104,7 @@ echo "========== Configuring Apache web server =========="
 echo "========== Creating a new Apache configuration file for Laravel =========="
 cat > /etc/apache2/sites-available/laravel.conf <<EOL
 <VirtualHost *:80>
-    ServerAdmin ayodejihamed@gmail.com
+    ServerAdmin $server_admin_email
     ServerName $server_ip
     DocumentRoot /var/www/html/laravel/public
 
@@ -143,13 +148,17 @@ else
     apt-get install -y git
 fi
 
+#######################################################################################################
+#######################################################################################################
+
+## Set up Laravel application
 # Navigate to the web root directory
 echo "========== Navigating to the web root directory =========="
 cd /var/www/html || exit
 
 # Clone the Laravel repository from GitHub
 echo "========== Cloning the Laravel repository from GitHub =========="
-git clone https://github.com/laravel/laravel.git
+git clone $laravel_repo_url
 
 # Navigate to the Laravel application directory
 echo "========== Navigating to the Laravel application directory =========="
@@ -158,7 +167,7 @@ cd laravel || exit
 # Install the Laravel application dependencies
 echo "========== Installing the Laravel application dependencies =========="
 composer install --no-interaction --optimize-autoloader --no-dev
-
+composer update --no-interaction --optimize-autoloader --no-dev
 # Set Laravel permissions
 echo "========== Setting Laravel permissions =========="
 chown -R www-data:www-data /var/www/html/laravel
@@ -174,28 +183,21 @@ cp .env.example .env
 echo "========== Generating an application key =========="
 php artisan key:generate
 
-# Database Setup
+# Mysql Database Setup
 echo "========== Database Setup =========="
-mysql -uroot -p"$mysql_root_password" -e "CREATE DATABASE laravel;"
+db_name="laravel"
+mysql_user="root"
 
-# Grant the database user full permissions to the database
-echo "========== Granting the database user full permissions to the database =========="
-mysql -uroot -p"$mysql_root_password" -e "GRANT ALL PRIVILEGES ON laravel.* TO 'root'@'localhost' IDENTIFIED BY '$mysql_root_password' WITH GRANT OPTION;"
-
-# Update the user privileges
-echo "========== Updating the user privileges =========="
-mysql -uroot -p"$mysql_root_password" -e "FLUSH PRIVILEGES;"
-mysql -uroot -p"$mysql_root_password" -e "EXIT;"
+mysql -u $mysql_user -p"$mysql_root_password" <<MYSQL_SCRIPT
+CREATE DATABASE $db_name;
+GRANT ALL PRIVILEGES ON $db_name.* TO '$mysql_user'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+MYSQL_SCRIPT
 
 # Update the .env file with the database connection details
 echo "========== Updating the .env file with the database connection details =========="
-sed -i "s/APP_ENV=local/APP_ENV=production/" .env
-sed -i "s/APP_DEBUG=true/APP_DEBUG=false/" .env
-sed -i "s/APP_URL=http://localhost/DB_HOST=http://$server_ip/" .env
-sed -i "s/DB_HOST=127.0.0.1/DB_HOST=$server_ip/" .env
-sed -i "s/DB_DATABASE=homestead/DB_DATABASE=laravel/" .env
-sed -i "s/DB_USERNAME=homestead/DB_USERNAME=root/" .env
-sed -i "s/DB_PASSWORD=secret/DB_PASSWORD=$mysql_root_password/" .env
+sed -i "s/DB_PASSWORD=.*/DB_PASSWORD='$mysql_root_password'/" .env
+
 
 # Cache the configuration values
 echo "========== Caching the configuration values =========="
@@ -209,19 +211,41 @@ php artisan migrate --force
 echo "========== Restarting Apache web server =========="
 systemctl restart apache2
 
+#######################################################################################################
+#######################################################################################################
+
 # End logging the script
 echo "========== Deployment ended at $(date) =========="
 
+#######################################################################################################
+#######################################################################################################
+
 # Add firewall rules
+
+#  Check if ufw is installed and active
+if ! dpkg -l | grep -q "ufw"; then
+    echo "ufw is not installed. Installing..."
+    apt-get install -y ufw
+    echo "ufw installed."
+fi
+
+# Enable ufw
+echo "========== Enabling ufw =========="
+ufw --force enable
+
 echo "========== Adding firewall rules =========="
 ufw allow openssh
 ufw allow 80/tcp
 ufw allow 443/tcp
 ufw allow 3306/tcp
-ufw --force enable
+
+#######################################################################################################
+#######################################################################################################
 
 # Access the application in a browser using the server IP address
 echo "========== Access the application in a browser using the server IP address =========="
 echo "========== Server IP address: $server_ip =========="
 
 # End of script
+
+#######################################################################################################
